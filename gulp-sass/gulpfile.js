c5dda423bc1216fcc32e2d26282eefecea1618cd
likimiks <likimiks@yandex.ru>
2020-03-18 20:34:44 +0300

    Фаилы верстки

---
+++ gulp-sass/gulpfile.js
@@ -0,0 +1,22 @@
+// Подключаем модули
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const watch = require('gulp-watch');
+
+// Создаем задание
+gulp.task('sass-compile', function(){
+
+	return gulp.src('./scss/**/*.scss')
+			   .pipe(sourcemaps.init())
+			   .pipe(sass().on('error', sass.logError))
+			   .pipe(sourcemaps.write('./'))
+			   .pipe(gulp.dest('./css/'));
+
+});
+
+gulp.task('watch', function(){
+
+	gulp.watch('./scss/**/*.scss', gulp.series('sass-compile'));
+
+});
